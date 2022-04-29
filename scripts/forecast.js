85ca0047e083a9cd35af4e4449b4707fa70c059b
ganeevsandhu <101134674+ganeevsandhu@users.noreply.github.com>
2022-04-29 15:42:13 +0530

    Add files via upload

---
+++ scripts/forecast.js
@@ -0,0 +1,24 @@
+class Forecast {
+    constructor() {
+        this.key = '82NlRjsVpu6iKWqxUEJnpWF72khVnk4X';
+        this.weatherURL = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURL = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city) {
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+        return { cityDets, weather }; //when property and value have same names, write in this fashion
+    }
+    async getCity(city) {
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURL + query);
+        const data = await response.json();
+        return data[0];
+    }
+    async getWeather(id) {
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.cityURL + query);
+        const data = await response.json();
+        return data[0];
+    }
+}
